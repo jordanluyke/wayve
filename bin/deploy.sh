4b1d3d2a666639ab0fabbc03545cf59904c2d2f4
Jordan Luyke <jordanluyke@gmail.com>
2016-08-01 20:13:02 -0700

    update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-git checkout gh-pages
-git add site -f && git commit -m "site update" && git subtree push --prefix site origin gh-pages
+npm start build
+git branch -D gh-pages
+git push origin --delete gh-pages
+git checkout -b gh-pages
+git add site -f
+git commit -m "site update"
+git subtree push --prefix site origin gh-pages
 git checkout master
+git branch -D gh-pages
